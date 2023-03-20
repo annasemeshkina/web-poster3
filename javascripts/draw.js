929a0409c5391873db7577fc3c16d652546a525a
smhkn <semeshkinaa@gmail.com>
2023-03-20 16:41:37 +0300

    7

---
+++ javascripts/draw.js
@@ -0,0 +1,30 @@
+let canvas = document.getElementById('art');
+let ctx = canvas.getContext('2d');
+ctx.strokeStyle = '#1a2edb';
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+function mouseMove(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  ctx.lineTo(mousePos.x, mousePos.y);
+  ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  ctx.beginPath();
+  ctx.moveTo(mousePos.x, mousePos.y);
+  evt.preventDefault();
+  canvas.addEventListener('mousemove', mouseMove, false);
+});
+
+
+canvas.addEventListener('mouseup', function() {
+  canvas.removeEventListener('mousemove', mouseMove, false);
+}, false);
